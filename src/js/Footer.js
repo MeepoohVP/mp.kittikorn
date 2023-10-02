ae9c283cf6eaf0c398e039b3a2fdc003ffd96ce4
poohvp <poohvp1147@gmail.com>
2023-10-02 16:09:09 +0700

    link footer

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -4,13 +4,13 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 function Footer() {
     return (
         <footer className="w-full h-auto bg-[#7971EA]/[.8] text-center pt-4 text-2xl text-white/[.65]">
-            <a href="#" className="mx-2">
+            <a href="https://www.facebook.com/meepooh.vp" className="mx-2">
               <i className="bi bi-facebook"></i>
             </a>
-            <a href="#" className="mx-2">
+            <a href="https://www.instagram.com/pooh_vp/" className="mx-2">
               <i className="bi bi-instagram"></i>
             </a>
-            <a href="#" className="mx-2">
+            <a href="https://github.com/MeepoohVP" className="mx-2">
               <i className="bi bi-github"></i>
             </a>
             <div className="w-full h-auto bg-[#7971EA] mt-2 md:text-base text-sm">© 2023 All Rights Reserved by Kittikorn Voraprateep</div>
